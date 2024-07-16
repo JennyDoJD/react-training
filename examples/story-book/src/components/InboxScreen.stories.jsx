e12003589a127ee83c28970f451d7f0be9e7be6d
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-16 09:44:55 +0700

    fix: import libs

--- examples/story-book/src/components/InboxScreen.stories.jsx
+++ examples/story-book/src/components/InboxScreen.stories.jsx
@@ -1,6 +1,6 @@
 import InboxScreen from './InboxScreen';
 import store from '../lib/store';
-import { http, HttpResponse } from 'msw';
+import { rest } from 'msw';
 import { MockedState } from './TaskList.stories';
 import { Provider } from 'react-redux';
 import {
@@ -21,17 +21,27 @@ export const Default = {
   parameters: {
     msw: {
       handlers: [
-        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
-          return HttpResponse.json(MockedState.tasks);
-        }),
+        rest.get(
+          'https://jsonplaceholder.typicode.com/todos?userId=1',
+          (req, res, ctx) => {
+            return res(ctx.json(MockedState.tasks));
+          }
+        ),
       ],
     },
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
+
+    // Waits for the component to transition from the loading state
     await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+
+    // Waits for the component to be updated based on the store
     await waitFor(async () => {
+      // Simulates pinning the first task
       await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+
+      // Simulates pinning the third task
       await fireEvent.click(canvas.getByLabelText('pinTask-3'));
     });
   },
@@ -41,11 +51,12 @@ export const Error = {
   parameters: {
     msw: {
       handlers: [
-        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
-          return new HttpResponse(null, {
-            status: 403,
-          });
-        }),
+        rest.get(
+          'https://jsonplaceholder.typicode.com/todos?userId=1',
+          (req, res, ctx) => {
+            return res(ctx.status(403));
+          }
+        ),
       ],
     },
   },
