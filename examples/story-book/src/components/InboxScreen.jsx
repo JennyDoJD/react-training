a74ecd613a4d2d380da2cc2ad419bb562d0a58b9
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-16 09:43:03 +0700

    fix: import react libs

--- examples/story-book/src/components/InboxScreen.jsx
+++ examples/story-book/src/components/InboxScreen.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from '../lib/store';
 import TaskList from './TaskList';
@@ -6,7 +6,6 @@ import TaskList from './TaskList';
 export default function InboxScreen() {
   const dispatch = useDispatch();
   const { error } = useSelector((state) => state.taskbox);
-
   useEffect(() => {
     dispatch(fetchTasks());
   }, []);
