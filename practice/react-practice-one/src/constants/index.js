59cb0027ccf54694353f491d1d57cfe615aa9dfd
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-19 16:14:32 +0700

    feat: define sizes of component and variants of text

---
+++ practice/react-practice-one/src/constants/index.js
@@ -0,0 +1,21 @@
+/**
+ * Define size of component
+ */
+export const COMPONENT_SIZES = {
+  default: 'default',
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extraLarge: 'xl',
+  doubleExtraLarge: '2xl',
+};
+
+/**
+ * Define variants of Text
+ */
+export const TEXT_VARIANTS = {
+  default: 'default',
+  title: 'title',
+  desc: 'desc',
+  name: 'name',
+};
