80004c0f3af8db517ae1279b2fbaad0a204de799
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-19 17:09:12 +0700

    feat: implement text common component

---
+++ practice/react-practice-one/src/components/Text/Text.stories.jsx
@@ -0,0 +1,27 @@
+import Text from './Text';
+import { COMPONENT_SIZES, TEXT_VARIANTS } from '../../constants';
+
+const meta = {
+  title: 'COMPONENTS/Common/Text',
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: Object.values(TEXT_VARIANTS),
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: Object.values(COMPONENT_SIZES),
+      },
+    },
+    children: { control: 'text' },
+  },
+};
+
+export default meta;
+
+export const Default = (args) => <Text {...args} />;
+Default.args = {};
