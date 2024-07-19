d970286a2d8d0a5464f5b232b83dc5e74d3fcc53
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-19 10:58:26 +0700

    feat: implement icons common component

---
+++ practice/react-practice-one/src/components/Icons/Icon.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AddIcon } from './AddIcon';
+
+export default {
+  title: 'Icons',
+};
+
+// AddIcon story
+export const Add = () => <AddIcon />;
+Add.storyName = 'Add Icon';
