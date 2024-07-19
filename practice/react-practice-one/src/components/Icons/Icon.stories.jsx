96aaa48d0b21dc6c32017f4f0cf6f0f8535f1346
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-19 12:42:25 +0700

    feat: create Storybook for icons component

---
+++ practice/react-practice-one/src/components/Icons/Icon.stories.jsx
@@ -0,0 +1,71 @@
+import AddIcon from './AddIcon';
+import DashboardIcon from './DashboardIcon';
+import DeleteIcon from './DeleteIcon';
+import DiscountIcon from './DiscountIcon';
+import EditIcon from './EditIcon';
+import HomeIcon from './HomeIcon';
+import LogoIcon from './LogoIcon';
+import LogoutIcon from './LogoutIcon';
+import MessageIcon from './MessageIcon';
+import NotificationIcon from './NotificationIcon';
+import SearchIcon from './SearchIcon';
+import SelectIcon from './SelectIcon';
+import SettingIcon from './SettingIcon';
+
+const meta = {
+  title: 'COMPONENTS/Common/Icons',
+};
+
+export default meta;
+
+export const Add = {
+  render: () => <AddIcon />,
+};
+
+export const Dashboard = {
+  render: () => <DashboardIcon />,
+};
+
+export const Delete = {
+  render: () => <DeleteIcon />,
+};
+
+export const Discount = {
+  render: () => <DiscountIcon />,
+};
+
+export const Edit = {
+  render: () => <EditIcon />,
+};
+
+export const Home = {
+  render: () => <HomeIcon />,
+};
+
+export const Logo = {
+  render: () => <LogoIcon />,
+};
+
+export const Logout = {
+  render: () => <LogoutIcon />,
+};
+
+export const Message = {
+  render: () => <MessageIcon />,
+};
+
+export const Notification = {
+  render: () => <NotificationIcon />,
+};
+
+export const Search = {
+  render: () => <SearchIcon />,
+};
+
+export const Select = {
+  render: () => <SelectIcon />,
+};
+
+export const Setting = {
+  render: () => <SettingIcon />,
+};
