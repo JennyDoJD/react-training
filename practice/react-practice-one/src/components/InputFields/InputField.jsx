350f72ad0ee9f31a42ee04a06690c1f9a0032521
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-22 15:39:37 +0700

    fix: import proptypes

--- practice/react-practice-one/src/components/InputFields/InputField.jsx
+++ practice/react-practice-one/src/components/InputFields/InputField.jsx
@@ -1,8 +1,18 @@
+/* Import dependencies */
+import PropTypes from 'prop-types';
+
 /* Import CSS */
 import './inputField.css';
 
-const InputField = () => {
-  return <input className="input-form" type="text"></input>;
+const InputField = ({ type, placeholder }) => {
+  return (
+    <input type={type} placeholder={placeholder} className="input-container" />
+  );
+};
+
+InputField.propTypes = {
+  type: PropTypes.oneOf(['search', 'number', 'text']).isRequired,
+  placeholder: PropTypes.string,
 };
 
 export default InputField;
