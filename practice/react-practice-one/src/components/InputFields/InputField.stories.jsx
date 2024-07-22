b1338bf3815d3d279bef32f52078b8bcd0f214d0
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-22 12:29:52 +0700

    feat: implement storyBook of the inputFields

---
+++ practice/react-practice-one/src/components/InputFields/InputField.stories.jsx
@@ -0,0 +1,11 @@
+import InputField from './InputField';
+
+const meta = {
+  title: 'COMPONENTS/Common/InputField',
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Input = (args) => <InputField {...args} />;
+Input.args = {};
