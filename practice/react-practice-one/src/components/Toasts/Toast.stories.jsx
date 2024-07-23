f977043de6493c9926833419a7173e32f52b9cf3
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-23 15:51:44 +0700

    fix: default value is in each prop

--- practice/react-practice-one/src/components/Toasts/Toast.stories.jsx
+++ practice/react-practice-one/src/components/Toasts/Toast.stories.jsx
@@ -3,13 +3,18 @@ import Toast from './Toast';
 const meta = {
   title: 'COMPONENTS/Common/Toasts',
   tags: ['autodocs'],
-  args: {
-    message: '',
-  },
 };
 
 export default meta;
 
-export const SuccessToast = () => <Toast type="success" />;
+export const SuccessToast = (args) => <Toast {...args} />;
+SuccessToast.args = {
+  type: 'success',
+  message: 'Succeed to add the product!',
+};
 
-export const ErrorToast = () => <Toast type="error" />;
+export const ErrorToast = (args) => <Toast {...args} />;
+ErrorToast.args = {
+  type: 'error',
+  message: 'Failed to add the product!',
+};
