a06f5cf8eb57adde35d780c524148c72906adde5
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-23 10:40:26 +0700

    fix: generate all states of the button component

--- practice/react-practice-one/src/components/Buttons/Button.stories.jsx
+++ practice/react-practice-one/src/components/Buttons/Button.stories.jsx
@@ -1,5 +1,6 @@
 import Button from './Button';
 import { BUTTON_VARIANTS } from '../../constants';
+import EditIcon from '../Icons/EditIcon';
 
 const meta = {
   title: 'COMPONENTS/Common/Buttons',
@@ -15,4 +16,17 @@ export const PrimaryButton = (args) => <Button {...args} />;
 PrimaryButton.args = {
   variant: BUTTON_VARIANTS.primary,
   label: 'Cancel',
+};
+
+export const SecondaryButton = (args) => <Button {...args} />;
+SecondaryButton.args = {
+  variant: BUTTON_VARIANTS.secondary,
+  label: 'Save',
+};
+
+export const ContainedButton = (args) => <Button {...args} />;
+ContainedButton.args = {
+  variant: BUTTON_VARIANTS.contained,
+  icon: <EditIcon />,
+  label: 'Edit',
 };
