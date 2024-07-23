a60657a5a93125f3d0e174351f77558efd9b2d62
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-23 15:17:00 +0700

    fix: import constants and deplendencies

--- practice/react-practice-one/src/components/SearchBar/SearchBar.jsx
+++ practice/react-practice-one/src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,26 @@
 import SearchIcon from '../Icons/SearchIcon';
 import InputField from '../InputFields/InputField';
 
+/* Import constants */
+import { PLACEHOLDER_TEXT } from '../../constants';
+
+/* Import dependencies */
+import PropTypes from 'prop-types';
+
 /* Import CSS */
 import './searchBar.css';
 
-const SearchBar = () => {
+const SearchBar = ({ placeholder }) => {
   return (
     <div className="search-container input-form">
       <SearchIcon />
-      <InputField type="search" placeholder="Search for food, coffee, etc.." />
+      <InputField type="search" placeholder={placeholder} />
     </div>
   );
+};
+
+SearchBar.propTypes = {
+  placeholder: PropTypes.oneOf(Object.values(PLACEHOLDER_TEXT)),
 };
 
 export default SearchBar;
