5ee5fb7a725a66232c17a2066b1c07209d3dca9a
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-23 12:33:49 +0700

    fix: import dependencies

--- practice/react-practice-one/src/components/Progress/Progress.jsx
+++ practice/react-practice-one/src/components/Progress/Progress.jsx
@@ -1,13 +1,20 @@
+/* Import dependencies */
+import PropTypes from 'prop-types';
+
 /* Import CSS */
 import './progress.css';
 
-const Progress = () => {
+const Progress = ({ text = 'Loading...' }) => {
   return (
     <div className="progress">
       <div className="progress-loading"></div>
-      <span className="visually-hidden">Loading...</span>
+      <span className="visually-hidden">{text}</span>
     </div>
   );
+};
+
+Progress.propTypes = {
+  text: PropTypes.string,
 };
 
 export default Progress;
