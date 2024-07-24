92156c80e2a8a3cca8a1d919b067ad4b3cee1b02
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-24 17:37:59 +0700

    fix: implement error case of TextFields component

--- practice/react-practice-one/src/components/TextFields/TextField.jsx
+++ practice/react-practice-one/src/components/TextFields/TextField.jsx
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types';
 /* Import CSS */
 import './textField.css';
 
-const TextField = ({ id, type, value, onBlur, onChange }) => {
+const TextField = ({ id, type, value, onBlur, onChange, errorMessage }) => {
   return (
-    <input
-      className="input-overlay"
-      type={type}
-      id={id}
-      value={value}
-      onBlur={onBlur}
-      onChange={onChange}
-    />
+    <div className="input-overlay">
+      <input
+        className={`input-content ${errorMessage ? 'input-error' : ''}`}
+        type={type}
+        id={id}
+        value={value}
+        onBlur={onBlur}
+        onChange={onChange}
+      />
+      {errorMessage && <span className="error-message">{errorMessage}</span>}
+    </div>
   );
 };
 
@@ -23,6 +26,7 @@ TextField.propTypes = {
   value: PropTypes.string,
   onBlur: PropTypes.func,
   onChange: PropTypes.func,
+  errorMessage: PropTypes.string,
 };
 
 export default TextField;
--- practice/react-practice-one/src/components/TextFields/TextField.stories.jsx
+++ practice/react-practice-one/src/components/TextFields/TextField.stories.jsx
@@ -1,3 +1,7 @@
+/* Import dependencies */
+import { useState } from 'react';
+
+/* Import components */
 import TextField from './TextField';
 
 const meta = {
@@ -7,11 +11,31 @@ const meta = {
 
 export default meta;
 
-export const Default = (args) => <TextField {...args} />;
+const Template = (args) => {
+  const [value, setValue] = useState(args.value || '');
+  const [error, setError] = useState(args.errorMessage || '');
+
+  const handleBlur = () => {
+    if (!value) {
+      setError('This field is required.');
+    } else {
+      setError('');
+    }
+  };
+
+  return (
+    <TextField
+      {...args}
+      value={value}
+      onBlur={handleBlur}
+      onChange={(e) => setValue(e.target.value)}
+      errorMessage={error}
+    />
+  );
+};
+
+export const Default = (args) => <Template {...args} />;
 Default.args = {
   id: '1',
   type: 'text',
-  value: 'Foods Management',
-  onChange: () => {},
-  onBlur: () => {},
 };
