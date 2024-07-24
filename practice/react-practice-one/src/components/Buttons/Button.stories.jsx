b9207d0fb1362d3f0c3949cac6c1975b42863dae
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-24 15:43:27 +0700

    fix: capitalize constants

--- practice/react-practice-one/src/components/Buttons/Button.stories.jsx
+++ practice/react-practice-one/src/components/Buttons/Button.stories.jsx
@@ -1,6 +1,5 @@
 import Button from './Button';
 import { BUTTON_VARIANTS } from '../../constants';
-import EditIcon from '../Icons/EditIcon';
 import { action } from '@storybook/addon-actions';
 
 const meta = {
@@ -15,22 +14,14 @@ export default meta;
 
 export const PrimaryButton = (args) => <Button {...args} />;
 PrimaryButton.args = {
-  variant: BUTTON_VARIANTS.primary,
+  variant: BUTTON_VARIANTS.PRIMARY,
   label: 'Cancel',
   onClick: action('primary-button-clicked'),
 };
 
 export const SecondaryButton = (args) => <Button {...args} />;
 SecondaryButton.args = {
-  variant: BUTTON_VARIANTS.secondary,
+  variant: BUTTON_VARIANTS.SECONDARY,
   label: 'Save',
   onClick: action('secondary-button-clicked'),
-};
-
-export const ContainedButton = (args) => <Button {...args} />;
-ContainedButton.args = {
-  variant: BUTTON_VARIANTS.contained,
-  icon: <EditIcon />,
-  label: 'Edit',
-  onClick: action('contained-button-clicked'),
 };
