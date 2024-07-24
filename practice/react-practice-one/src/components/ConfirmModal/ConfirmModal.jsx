1ed13e8a022cc43248b94cb5a7ba44ef3ebff3bc
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-24 16:21:41 +0700

    fix: remove redundant props

--- practice/react-practice-one/src/components/ConfirmModal/ConfirmModal.jsx
+++ practice/react-practice-one/src/components/ConfirmModal/ConfirmModal.jsx
@@ -11,11 +11,11 @@ import { MODAL_TITLES } from '../../constants';
 import Text from '../Text/Text.jsx';
 import Button from '../Buttons/Button.jsx';
 
-const ConfirmModal = ({ id, title, onSubmit, onClose, isMutating, isOpen }) => {
+const ConfirmModal = ({ title, onSubmit, onClose, isOpen }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay" id={id}>
+    <div className="modal-overlay">
       <div className="modal-content">
         <Text className="modal-title">{title}</Text>
         <div className="modal-actions">
@@ -24,14 +24,12 @@ const ConfirmModal = ({ id, title, onSubmit, onClose, isMutating, isOpen }) => {
             label="Cancel"
             onClick={onClose}
             className="btn-confirm"
-            disabled={isMutating}
           />
           <Button
             variant="primary"
             label="Yes"
             onClick={onSubmit}
             className="btn-confirm"
-            disabled={isMutating}
           />
         </div>
       </div>
@@ -40,12 +38,10 @@ const ConfirmModal = ({ id, title, onSubmit, onClose, isMutating, isOpen }) => {
 };
 
 ConfirmModal.propTypes = {
-  id: PropTypes.string.isRequired,
   label: PropTypes.string,
   title: PropTypes.oneOf(Object.values(MODAL_TITLES)).isRequired,
   onClose: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
-  isMutating: PropTypes.bool,
   isOpen: PropTypes.bool,
 };
 
