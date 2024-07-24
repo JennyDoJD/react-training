75035a8356d07acf4f20d4730b8020c09557f043
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-24 15:11:02 +0700

    fix: add argTypes for SearchBar component in Storybook

--- practice/react-practice-one/src/components/SearchBar/SearchBar.stories.jsx
+++ practice/react-practice-one/src/components/SearchBar/SearchBar.stories.jsx
@@ -4,11 +4,14 @@ import { PLACEHOLDER_TEXT } from '../../constants';
 const meta = {
   title: 'COMPONENTS/Common/SearchBar',
   tags: ['autodocs'],
+  argTypes: {
+    placeholder: { control: 'text' },
+  },
 };
 
 export default meta;
 
 export const Default = (args) => <SearchBar {...args} />;
 Default.args = {
-  placeholder: PLACEHOLDER_TEXT.primary,
+  placeholder: PLACEHOLDER_TEXT.DEFAULT,
 };
