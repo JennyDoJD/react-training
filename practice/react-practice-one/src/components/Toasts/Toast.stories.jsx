c0931d368eb40ea09c126b64cfdb869e5b85ea0e
JennyDoJD <JennyDoJD@loido.ued@gmail.com>
2024-07-25 15:57:17 +0700

    fix: add description for name and message of argTypes

--- practice/react-practice-one/src/components/Toasts/Toast.stories.jsx
+++ practice/react-practice-one/src/components/Toasts/Toast.stories.jsx
@@ -1,21 +1,35 @@
+/* Import components */
 import Toast from './Toast';
-import { MESSAGES } from '../constants/message';
+
+/* Import constants */
+import { MESSAGES, STATUS } from '../../constants';
 
 const meta = {
   title: 'COMPONENTS/Common/Toasts',
   tags: ['autodocs'],
+  argTypes: {
+    name: {
+      control: { type: 'select' },
+      options: Object.values(STATUS),
+      description: 'This value specifies the status of the Toast',
+    },
+    message: {
+      control: { type: 'text' },
+      description: 'This value specifies the message of the Toast',
+    },
+  },
 };
 
 export default meta;
 
 export const SuccessToast = (args) => <Toast {...args} />;
 SuccessToast.args = {
-  name: 'success',
+  name: STATUS.SUCCESS,
   message: MESSAGES.ADD_PRODUCT_SUCCESS_MESSAGE,
 };
 
 export const ErrorToast = (args) => <Toast {...args} />;
 ErrorToast.args = {
-  name: 'error',
+  name: STATUS.ERROR,
   message: MESSAGES.ADD_PRODUCT_FAILED_MESSAGE,
 };
