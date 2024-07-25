1803b9bbbaf9adb6c16ac1290fe0c54d72803a2d
JennyDoJD <loido.ued@gmail.com>
2024-07-25 07:56:28 +0700

    feat: implement SideBar components

--- practice/react-practice-one/src/components/Sidebar/Sidebar.jsx
+++ practice/react-practice-one/src/components/Sidebar/Sidebar.jsx
@@ -1,48 +1,95 @@
+/* Import dependencies */
+import React, { useState } from 'react';
+
+/* Import components */
+import LogoIcon from '../Icons/LogoIcon';
 import HomeIcon from '../Icons/HomeIcon';
 import DiscountIcon from '../Icons/DiscountIcon';
 import DashboardIcon from '../Icons/DashboardIcon';
 import MessageIcon from '../Icons/MessageIcon';
-import NotificationIcon from '../Icons/MessageIcon';
+import NotificationIcon from '../Icons/NotificationIcon';
 import SettingIcon from '../Icons/SettingIcon';
 import LogoutIcon from '../Icons/LogoutIcon';
 
-const SideBar = () => {
+/* Import CSS */
+import './sideBar.css';
+
+const SideBar = ({ onClick, activeItem, setActiveItem }) => {
+  const handleClick = (item) => {
+    setActiveItem(item);
+    if (onClick) onClick(item);
+  };
+
   return (
     <aside className="sidebar-overlay">
       <div className="sidebar-header">
-        <a href="index.html" className="sidebar-logo">
-          <img src="./assets/images/logo.svg" alt="logo" />
+        <a href="javascript:void(0)" className="sidebar-logo">
+          <LogoIcon />
         </a>
       </div>
       <div className="sidebar-body">
         <ul className="nav-list">
-          <li className="nav-item active">
-            <a href="index.html" className="nav-link">
+          <li className={`nav-item ${activeItem === 'home' ? 'active' : ''}`}>
+            <a
+              href="javascript:void(0)"
+              className="nav-link"
+              onClick={() => handleClick('home')}
+            >
               <HomeIcon />
             </a>
           </li>
-          <li className="nav-item">
-            <a href="javascript:void(0)" className="nav-link">
-              <DiscountIcon />
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="javascript:void(0)" className="nav-link">
+          <li
+            className={`nav-item ${activeItem === 'dashboard' ? 'active' : ''}`}
+          >
+            <a
+              href="javascript:void(0)"
+              className="nav-link"
+              onClick={() => handleClick('dashboard')}
+            >
               <DashboardIcon />
             </a>
           </li>
-          <li className="nav-item">
-            <a href="javascript:void(0)" className="nav-link">
+          <li
+            className={`nav-item ${activeItem === 'discount' ? 'active' : ''}`}
+          >
+            <a
+              href="javascript:void(0)"
+              className="nav-link"
+              onClick={() => handleClick('discount')}
+            >
+              <DiscountIcon />
+            </a>
+          </li>
+          <li
+            className={`nav-item ${activeItem === 'message' ? 'active' : ''}`}
+          >
+            <a
+              href="javascript:void(0)"
+              className="nav-link"
+              onClick={() => handleClick('message')}
+            >
               <MessageIcon />
             </a>
           </li>
-          <li className="nav-item">
-            <a href="javascript:void(0)" className="nav-link">
+          <li
+            className={`nav-item ${activeItem === 'notification' ? 'active' : ''}`}
+          >
+            <a
+              href="javascript:void(0)"
+              className="nav-link"
+              onClick={() => handleClick('notification')}
+            >
               <NotificationIcon />
             </a>
           </li>
-          <li className="nav-item">
-            <a href="javascript:void(0)" className="nav-link">
+          <li
+            className={`nav-item ${activeItem === 'setting' ? 'active' : ''}`}
+          >
+            <a
+              href="javascript:void(0)"
+              className="nav-link"
+              onClick={() => handleClick('setting')}
+            >
               <SettingIcon />
             </a>
           </li>
