5bb519bceb22bd980aeba4f8698c0eb6cf2f2f52
JennyDoJD <loido.ued@gmail.com>
2024-07-29 06:29:55 +0700

    feat: implement ProductForm component

---
+++ practice/react-practice-one/src/components/ProductForm/ProductForm.jsx
@@ -0,0 +1,120 @@
+/* Import dependencies */
+import PropTypes from 'prop-types';
+
+/* Import components */
+import Text from '../Text/Text';
+import Button from '../Buttons/Button';
+import TextField from '../TextFields/TextField';
+
+/* Import CSS */
+import './productForm.css';
+import '../Text/text.css';
+
+const ProductForm = ({
+  onSave,
+  onCancel,
+  onChange,
+  formData,
+  errors,
+  headingPage,
+}) => {
+  return (
+    <div className="form-overlay">
+      <div className="form-container">
+        <Text variant="title" className="text-title-default form-heading ">
+          {headingPage}
+        </Text>
+
+        <form className="form-control">
+          <div className="form-group">
+            <Text variant="title" className="text-title-sm">
+              Name
+            </Text>
+            <TextField
+              id="name"
+              type="text"
+              value={formData.name}
+              onChange={onChange}
+              errorMessage={errors.name}
+            />
+          </div>
+
+          <div className="form-group">
+            <Text variant="title" className="text-title-sm">
+              Price
+            </Text>
+            <TextField
+              id="price"
+              type="number"
+              value={formData.price}
+              onChange={onChange}
+              errorMessage={errors.price}
+            />
+          </div>
+
+          <div className="form-group">
+            <Text variant="title" className="text-title-sm">
+              Image URL
+            </Text>
+            <TextField
+              id="image-url"
+              type="text"
+              value={formData.imageUrl}
+              onChange={onChange}
+              errorMessage={errors.imageUrl}
+            />
+          </div>
+
+          <div className="form-group form-field-quantity">
+            <Text variant="title" className="text-title-sm">
+              Quantity
+            </Text>
+            <TextField
+              id="quantity"
+              type="number"
+              value={formData.quantity}
+              onChange={onChange}
+              errorMessage={errors.quantity}
+            />
+          </div>
+
+          <div className="form-button">
+            <Button
+              variant="primary"
+              label="Cancel"
+              onClick={onCancel}
+              className="btn-confirm"
+            />
+            <Button
+              variant="secondary"
+              label="Save"
+              onClick={onSave}
+              className="btn-confirm"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+ProductForm.propTypes = {
+  headingPage: PropTypes.string.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  formData: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.string,
+    imageUrl: PropTypes.string,
+    quantity: PropTypes.string,
+  }).isRequired,
+  errors: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.string,
+    imageUrl: PropTypes.string,
+    quantity: PropTypes.string,
+  }),
+};
+
+export default ProductForm;
---
+++ practice/react-practice-one/src/components/ProductForm/ProductForm.stories.jsx
@@ -0,0 +1,77 @@
+/* Import dependencies */
+import { action } from '@storybook/addon-actions';
+
+/* Import components */
+import ProductForm from './ProductForm';
+
+const meta = {
+  title: 'COMPONENTS/Common/ProductForm',
+  tags: ['autodocs'],
+  argTypes: {
+    onSave: {
+      action: 'save-clicked',
+      description: 'Function will be called when the save button is clicked',
+    },
+    onCancel: {
+      action: 'cancel-clicked',
+      description: 'Function will be called when the cancel button is clicked',
+    },
+    onChange: {
+      action: 'change',
+      description: 'Function will be called when the form input value changes',
+    },
+    formData: {
+      control: 'object',
+      description: 'This value specifies the form data',
+    },
+    errors: {
+      control: 'object',
+      description: 'This value specifies the form errors',
+    },
+    headingPage: {
+      type: 'text',
+    },
+  },
+};
+
+export default meta;
+
+export const Default = (args) => <ProductForm {...args} />;
+Default.args = {
+  headingPage: 'Create a new product',
+  onSave: action('save-clicked'),
+  onCancel: action('cancel-clicked'),
+  onChange: action('change'),
+  formData: {
+    name: '',
+    price: '',
+    imageUrl: '',
+    quantity: '',
+  },
+  errors: {
+    name: '',
+    price: '',
+    imageUrl: '',
+    quantity: '',
+  },
+};
+
+export const FormWithErrors = (args) => <ProductForm {...args} />;
+FormWithErrors.args = {
+  headingPage: 'Create a new product',
+  onSave: action('save-clicked'),
+  onCancel: action('cancel-clicked'),
+  onChange: action('change'),
+  formData: {
+    name: '',
+    price: '',
+    imageUrl: '',
+    quantity: '',
+  },
+  errors: {
+    name: 'Name is required',
+    price: 'Price must be a number',
+    imageUrl: 'Image URL is required',
+    quantity: 'Quantity must be a number',
+  },
+};
