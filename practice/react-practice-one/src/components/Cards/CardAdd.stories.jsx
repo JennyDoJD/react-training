7e39459e8826609a2ae17da4b252f13afc382660
JennyDoJD <loido.ued@gmail.com>
2024-07-29 04:39:58 +0700

    fix: create CardAdd storybook

---
+++ practice/react-practice-one/src/components/Cards/CardAdd.stories.jsx
@@ -0,0 +1,32 @@
+/* Import dependencies */
+import { action } from '@storybook/addon-actions';
+
+/* Import constants */
+import { CARD_TYPES } from '../../constants';
+
+/* Import components */
+import Card from './Card';
+
+const meta = {
+  title: 'COMPONENTS/Common/Card',
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: { type: 'text' },
+      description: 'Type of the card to render',
+    },
+    onAddClick: {
+      action: 'add-clicked',
+      description: 'Callback function for add button click',
+    },
+  },
+};
+
+export default meta;
+
+export const CardAdd = (args) => <Card {...args} />;
+CardAdd.args = {
+  type: CARD_TYPES.ADD,
+  text: 'Add New Dish',
+  onAddClick: action('add-clicked'),
+};
